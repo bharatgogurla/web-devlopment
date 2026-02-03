447eefba6131b6547e98e91f95812c195bee4517
bharatgogurla <bharatgogurla@gmail.com>
2026-02-03 23:39:58 +0530

    lec41 completed

---
+++ lec41/script.js
@@ -0,0 +1,183 @@
+//Arithmetic Opertor
+
+// let a = 12;
+// let b = 6;
+
+// let add = a + b;
+// console.log(add);
+
+// let subtract = a - b;
+// console.log(subtract);
+
+// let multiply = a * b;
+// console.log(multiply);
+
+// let divide = a / b;
+// console.log(divide);
+
+// let modulus = a % b;
+// console.log(modulus);
+
+// let exponent = a ** b;
+// console.log(exponent);
+
+// Assignment operator
+
+// let a = 10;
+// let b = 2;
+
+// let add = a += b;
+// console.log(add);
+
+// let subtract = a -= b;
+// console.log(subtract);
+
+// let multiply = a *= b;
+// console.log(multiply);
+
+// let divide = a /= b;
+// console.log(divide);
+
+// Comparision Opertor
+// let a = 25;
+// let b = 20;
+
+// let equal = a == "25";
+// console.log(equal);
+
+// let equalStrict = a === "25";
+// console.log(equalStrict);
+
+// let notEqual = a != "25";
+// console.log(notEqual);
+
+// let notEqualStrict = a !== "25";
+// console.log(notEqualStrict);
+
+// let greaterThan = a > b;
+// console.log(greaterThan);
+
+// let lesserThan = a < b;
+// console.log(lesserThan);
+
+// let greaterThanOrEqual = a >= 25;
+// console.log(greaterThanOrEqual);
+
+// let lesserThanOrEqual = b <= 20;
+// console.log(lesserThanOrEqual);
+
+// Ternary Operator
+// let age = 25;
+
+// let vote = age > 18 ? "can-vote" : "cannot-vote";
+// console.log(vote);
+
+// Logical Operator
+// let and = (true && true);
+// console.log(and);
+
+// let aand = (true && false);
+// console.log(aand);
+
+// let or = (false && false);
+// console.log(or);
+
+// let oor = (false || true);
+// console.log(oor);
+
+// let not = !(false);
+// console.log(not);
+
+// let nnot = !(true);
+// console.log(nnot);
+
+// Bitwise Operator
+// let a = 5;
+// let b = 2;
+
+// and = a & b;
+// console.log(and);
+
+// or = a | b;
+// console.log(or);
+
+// not = ~0;
+// console.log(not);
+
+// xor = a ^ b;
+// console.log(xor);
+
+// leftshift = a << 2;
+// console.log(leftshift);
+
+// rightshift = a << 1;
+// console.log(rightshift);
+
+// Conditional's
+// if
+// let age = 25;
+
+// if (age>18){
+//     console.log("can vote");
+// }
+
+// if-else
+// let age = 25;
+
+// if (age>18){
+//     console.log("can vote");
+// }
+// else{
+//     console.log("cannot vote");
+// }
+
+// else-if ladder
+// let marks = 70;
+
+// if (marks < 40){
+//     console.log("fail");
+// }
+// else if(marks >= 60){
+//     console.log("pass");
+// }
+
+// nestted if-else
+// let age = 25;
+
+// if (age > 0){
+//     if(age > 18){
+//         console.log("can vote");
+//     }
+//     else{
+//         console.log("cannot vote");
+//     }
+// }
+
+// switch
+// let day = 5;
+
+// switch(day){
+//     case 1:
+//         console.log("sunday");
+//         break;
+//     case 2:
+//         console.log("monday");
+//         break;
+//     case 3:
+//         console.log("tuesday");
+//         break;
+//     case 4:
+//         console.log("wednesday");
+//         break;
+//     case 5:
+//         console.log("thursday");
+//         break;
+//     case 6:
+//         console.log("friday");
+//         break;
+//     case 7:
+//         console.log("saturday");
+//         break;
+//     default:
+//         console.log("invalid number");
+// }
