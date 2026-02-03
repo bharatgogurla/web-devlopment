9de5236ca34293eaf6008187a773143e929bbd12
bharatgogurla <bharatgogurla@gmail.com>
2026-02-03 16:32:09 +0530

    lec40 completed

---
+++ lec40/script.js
@@ -0,0 +1,71 @@
+// var
+// gloabal scoped
+// var age = 25;
+// if(true) {
+//     console.log(age);
+// }
+
+// function scoped
+// function solve() {
+//     var age = 20;
+//    console.log(age);
+// }
+// console.log(age) -> not accessible
+// solve();
+
+// reassignment
+// var age = 20;
+// age = 25;
+
+//redeclaration
+// var age = 20;
+// var age = 36;
+
+// let
+// block scoped
+// {
+//     let a = 10;
+//     console.log(a);
+// }
+
+// can reassign
+// let a = 12;
+// a = 13;
+
+// cannot redeclare
+// let a = 20;
+// let a = 32;
+
+// const
+// block scoped
+// {
+//     area = 25;
+//     console.log(area);
+// }
+
+// cannot reassign
+// const area = 25;
+// area = 30;
+
+// cannot redeclare
+// const area = 25;
+// const area = 30;
+
+// primitive data types
+// number float string boolean
+// let marks = 20;
+// marks = 20.34;
+// marks = "bharat gogurla"
+// marks = true;
+
+// undefined
+// let marks
+// console.log(marks);
+
+// null
+// let marks = null;
+// console.log(marks);
+
+// BigInt
+// let marks = 12246786765652763473476858235485876347868674579247876347824972375673648764378626347698476876487698468976984678468763478639649862984769629876478647864978649786784697864978638624786827369846896489723646;
+// console.log(marks);
