52f8bf26b06affe39c247323d5f460bf90bf747d
bharatgogurla <bharatgogurla@gmail.com>
2026-02-05 20:08:58 +0530

    lec42 completed

---
+++ lec42/script.js
@@ -0,0 +1,122 @@
+// For Loop
+// for (let i = 1; i <= 10; i++) {
+//   console.log("bharat");
+// }
+
+// for (let i = 1; i <= 5; i++) {
+//   console.log(i);
+// }
+
+// for (let i = 5; i >= 1; i--) {
+//   console.log(i);
+// }
+
+// break
+// for (let i = 1; i <= 6; i++) {
+//   if (i == 4) {
+//     break;
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// continue
+// for (let i = 1; i <= 6; i++) {
+//   if (i == 3) {
+//     continue;
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// while loop
+// let i = 1;
+// while (i <= 10) {
+//   console.log("bharat");
+//   i++;
+// }
+
+// let i = 1;
+// while (i <= 5) {
+//   console.log(i);
+//   i++;
+// }
+
+// let i = 5;
+// while (i >= 1) {
+//   console.log(i);
+//   i--;
+// }
+
+// let i = 1;
+// while (i <= 6) {
+//   if (i == 4) {
+//     break;
+//   } else {
+//     console.log(i);
+//     i++;
+//   }
+// }
+
+// let i = 1;
+// while (i <= 6) {
+//   if (i == 3) {
+//     i++;
+//     continue;
+//   } else {
+//     console.log(i);
+//     i++;
+//   }
+// }
+
+// do while
+// let i = 1;
+// do {
+//   console.log("bharat");
+//   i++;
+// } while (i <= 10);
+
+// let i = 1;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 5);
+
+// let i = 5;
+// do {
+//   console.log(i);
+//   i--;
+// } while (i >= 1);
+
+// strings
+// let firstName = "bharat";
+// let lastName = "gogurla";
+
+// let name = `bharat
+// gogurla`;
+// console.log(typeof firstName);
+// console.log(name);
+
+// let firstName = new String("Bharat Gogurla");
+// console.log(firstName);
+
+// let op1 = "Bharat ";
+// let op2 = "Gogurla ";
+
+// let ans = op1 + op2;
+// console.log(ans);
+
+// let name = `${op1}${op2}`;
+// console.log(name);
+
+// console.log(op2.length);
+// console.log(op1.toUpperCase());
+// console.log(op2.toLowerCase());
+
+// let str = "bharat";
+// console.log(str.substring(2, 4));
+
+// let sentence = "hey guys what is up!";
+// let words = sentence.split(" ");
+// console.log(words);
+// console.log(words.join("-"));
