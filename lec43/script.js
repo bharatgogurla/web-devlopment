ab231245243896f24cdb633b8632f98d723ec850
bharatgogurla <bharatgogurla@gmail.com>
2026-02-06 19:59:31 +0530

    lec43 completed

---
+++ lec43/script.js
@@ -0,0 +1,71 @@
+// Function defination without parameter
+// function sayMyName() {
+//   console.log("Bharat Gogurla");
+// }
+
+// Function call
+// sayMyName();
+
+// function printCounting() {
+//   for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+//   }
+// }
+
+// printCounting();
+
+// Function with parameter
+// function printNumber(num) {
+//   console.log("print number:", num);
+// }
+
+// printNumber(2);
+
+// function getAverage(num1, num2) {
+//   let avg = (num1 + num2) / 2;
+//   console.log("average:", avg);
+// }
+
+// getAverage(4,6);
+
+// Return function
+// function getSum(a, b, c) {
+//   let sum = a + b + c;
+//   return sum;
+//   //unreachable code
+// }
+
+// let ans = getSum(1, 2, 3);
+// console.log(ans);
+
+// function getMyName(firstname, lastname) {
+//   let name = firstname + " " + lastname;
+//   return name;
+// }
+
+// fullname = getMyName("bharat", "gogurla");
+// console.log("full name:", fullname);
+
+// Function expression
+// const getMultiplication = function (a, b) {
+//   return a * b;
+// };
+
+// let ans = getMultiplication(2, 4);
+// console.log(ans);
+
+// let squareNumber = function (num) {
+//   let ans = num ** 2;
+//   return ans;
+// };
+
+// let square = squareNumber(3);
+// console.log(square);
+
+// Arrow function with parameter
+// let getExp = (x, y) => {
+//   let ans = x ** y;
+//   return ans;
+// };
+
+// console.log(getExp(2, 3));
