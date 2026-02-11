e99d5cbadb4460bcb91ca2c668cbdfd8cbc6a418
bharatgogurla <bharatgogurla@gmail.com>
2026-02-11 17:44:03 +0530

    lec48 completed

---
+++ lec48/script.js
@@ -0,0 +1,31 @@
+// Math object
+// console.log(Math.PI);
+
+// console.log(Math.max(60, 30, 43, 56, 23));
+
+// console.log(Math.min(12, 34, 45, 56, 87));
+
+// console.log(Math.round(3.6));
+
+// console.log(Math.floor(1.7));
+
+// console.log(Math.ceil(1.7));
+
+// console.log(Math.abs(-5));
+
+// console.log(Math.random());
+
+// console.log(Math.sqrt(5));
+
+// console.log(Math.pow(2, 10));
+
+// Date object
+// let curr = new Date();
+// console.log(curr);
+
+// let date = new Date("April 25 2006 06:15");
+// console.log(date);
+
+// let newDate = new Date(2006, 3, 26, 3);
+// console.log(newDate);
+// console.log(newDate.getFullYear());
