97b33dd6815b25eb79f887906b1f330967a4a11d
bharatgogurla <bharatgogurla@gmail.com>
2026-02-12 17:15:53 +0530

    lec49 completed

---
+++ lec49/script.js
@@ -0,0 +1,61 @@
+// Add value in object
+// let obj = {
+//   age: 12,
+//   wt: 73,
+//   ht: 175,
+// };
+
+// console.log(obj);
+// obj.color = "white";
+// console.log(obj);
+
+// Object cloning in js
+// Spread Operator
+// let src = {
+//   age: 19,
+//   wt: 72,
+//   ht: 175,
+// };
+
+// let dest = { ...src };
+// src.age = 20;
+// console.log("src:", src);
+// console.log("dest:", dest);
+
+// Assign operator
+// let src = {
+//   age: 19,
+//   ht: 175,
+//   wt: 72,
+// };
+
+// let src2 = {
+//   fName: "Bharat",
+//   lName: "Gogurla",
+// };
+
+// let dest = Object.assign({}, src, src2);
+// src.age = 20;
+// console.log(src);
+// console.log(dest);
+
+// Iteration
+// let src = {
+//   age: 12,
+//   wt: 68,
+//   ht: 180,
+// };
+
+// let dest = {};
+// // cloning using iteration
+// for (let key in src) {
+//   let newKey = key;
+//   let newValue = src[key];
+//   //insert newKey and value in dest and create a clone
+//   dest[newKey] = newValue;
+// }
+
+// src.age = 34;
+
+// console.log(src);
+// console.log(dest);
