0a96dc8cefc5e32e09f7dba0456c56086248370b
bharatgogurla <bharatgogurla@gmail.com>
2026-02-12 23:41:26 +0530

    lec50 completed

---
+++ lec50/script.js
@@ -0,0 +1,42 @@
+// Compile time error
+// Syntax error
+// console.log(1;
+
+// Runtime error
+// Reference error
+// console.log(x);
+
+// try {
+//   console.log("try block starts here");
+//   console.log(x);
+//   console.log("try block ends here");
+//   // a
+
+//   // b
+
+//   // c
+// } catch (err) {
+//   // we define here how we'll handle the error
+//   // retry logic
+//   // fallback mechanism
+//   // logging
+//   // custom error
+//   console.log("I am inside catch block");
+//   console.log("Your error is here", err);
+// } finally {
+//   console.log("I will run everytime, as i am finally block");
+// }
+
+// Let's create custom error
+// try {
+//   // Reference error
+//   console.log(x);
+// } catch (err) {
+//   throw new Error("First declare then print value");
+// }
+
+// Error code
+// let errorCode = 100;
+// if (errorCode == 100) {
+//   throw new Error("Invalid Json");
+// }
