193b93ef9e7d229ce68945e41aef445137eed3a9
bharatgogurla <bharatgogurla@gmail.com>
2026-02-14 17:02:48 +0530

    lec51 completed

---
+++ lec51/script.js
@@ -0,0 +1,15 @@
+// Access
+// let mydiv = document.querySelector("#mydiv");
+
+// Update
+// let newElement = document.createElement("span");
+// newElement.textContent = "Bharat Gogurla";
+
+// Insert
+// // Positions: "beforebegin", "afterbegin", "beforeend", "afterend"
+// mydiv.insertAdjacentElement("beforebegin", newElement);
+
+// Delete
+// let parent = document.querySelector("#mydiv");
+// let child = document.querySelector("#fpara");
+// parent.removeChild(child);
