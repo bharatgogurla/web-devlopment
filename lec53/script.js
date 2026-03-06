3555a2da6da499c9f926c0b4f0551c7bbad53683
bharatgogurla <bharatgogurla@gmail.com>
2026-03-06 23:53:39 +0530

    lec53 completed

---
+++ lec53/script.js
@@ -0,0 +1,44 @@
+// function changeText(event) {
+//   console.log(event);
+//   let fpara = document.getElementById("fpara");
+//   fpara.textContent = "Hello world";
+// }
+
+// let fpara = document.getElementById("fpara");
+
+// fpara.addEventListener('click', changeText);
+
+
+
+// let anchorElement = document.getElementById('fanchor');
+
+// function preventAction(event){
+//     event.preventDefault();
+//     anchorElement.textContent = "click change";
+// }
+
+// anchorElement.addEventListener('click',preventAction);
+
+
+
+// let paras = document.querySelectorAll('p');
+
+// function alertPara(event){
+//      alert("you have Clicked on para " + event.target.textContent);
+// }
+// for(let i=0; i<paras.length; i++){
+//     let para = paras[i];
+//     para.addEventListener('click', alertPara);
+// }
+
+
+
+// let myDiv = document.getElementById('wrapper');
+
+// function alertPara(event){
+//     if(event.target.nodeName === 'SPAN'){
+//         alert("You have Clicked on para " + event.target.textContent);
+//     }
+// }
+
+// document.addEventListener('click', alertPara);
