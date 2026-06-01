76412e505f0f4fdf38397a4c426a23870074c45d
bharatgogurla <bharatgogurla@gmail.com>
2026-06-01 11:22:50 +0530

    lec74 completed

---
+++ lec74/hooks-state/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div>The count is {count}</div>
+     <button onClick={()=>{setCount(count+1)}}>Click</button>
+    </>
+  )
+}
+
+export default App
